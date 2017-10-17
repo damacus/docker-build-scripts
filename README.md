257b7701a0403e047b32a8dda62837ba114c59d1
Dan Webb <dan.webb@damacus.io>
2017-10-17 22:23:15 +0100

    Update build scripts so some can be optional (e.g. DATE)

--- README.md
+++ README.md
@@ -4,16 +4,31 @@ Common build scripts & functions
 
 ## Usage
 
-Export the following variables (e.g. in variables.sh)
+Export the following variables e.g. in something like variables.sh.
+
+### Mandatory Arguments
+
+````shell
+PROJECT
+MAINTAINER
+DESCRIPTION
+```
+
+### Optional Arguments
 
 ```shell
-export PROJECT="${NAME:-$CIRCLE_PROJECT_REPONAME}"
-export DESCRIPTION="Docker container composer on PHP 5.5"
-export MAINTAINER="damacus"
 export VCS_URL="https://github.com/${MAINTAINER:?}/${PROJECT:?}"
 export DATE=$(date +%Y-%m-%dT%T%z)
 export COMMIT=$(git rev-parse --short HEAD)
 export FILE="Dockerfile"
-```
+````
 
-`wget "https://raw.githubusercontent.com/damacus/docker-build-scripts/master/functions.sh" > ".docker/functions.sh"`
+Download an source the script:
+
+```shell
+if ! [[ -e .docker/functions.sh ]];then
+  wget "https://raw.githubusercontent.com/damacus/docker-build-scripts/master/functions.sh" > ".docker/functions.sh"
+fi
+
+source `.docker/functions.sh`
+```
--- functions.sh
+++ functions.sh
@@ -1,21 +1,42 @@
 #!/bin/sh
 set +o pipefail
 
+# If the following variables are not set then set them
+if [[ -z $PROJECT ]];then
+  PROJECT="${NAME:-$CIRCLE_PROJECT_REPONAME}"
+fi
+if [[ -z $VCS_URL ]];then
+  VCS_URL="https://github.com/${MAINTAINER:?}/${PROJECT:?}"
+fi
+if [[ -z $DATE ]];then
+  DATE=$(date +%Y-%m-%dT%T%z)
+fi
+if [[ -z $COMMIT ]];then
+  COMMIT=$(git rev-parse --short HEAD)
+fi
+if [[ -z $FILE ]];then
+  FILE="Dockerfile"
+fi
+#You also need to set
+#PROJECT
+#MAINTAINER
+#DESCRIPTION
+
 build() {
   # Only load and save cache in CI environment
   if [[ -e /caches/app.tar ]];then
     docker load -i /caches/app.tar
   fi
 
-  docker build --cache-from="${MAINTAINER}"/"${PROJECT}" \
-               --build-arg PROJECT="${PROJECT}" \
-               --build-arg MAINTAINER="${MAINTAINER}" \
-               --build-arg URL="${VCS_URL}" \
-               --build-arg DATE="${DATE}" \
-               --build-arg COMMIT="${COMMIT}" \
-               --build-arg DESCRIPTION="${DESCRIPTION}" \
-               --file "${FILE}" \
-               --tag "${MAINTAINER}"/"${PROJECT}" .
+  docker build --cache-from="${MAINTAINER:?}"/"${PROJECT:?}" \
+               --build-arg PROJECT="${PROJECT:?}" \
+               --build-arg MAINTAINER="${MAINTAINER:?}" \
+               --build-arg URL="${VCS_URL:?}" \
+               --build-arg DATE="${DATE:?}" \
+               --build-arg COMMIT="${COMMIT:?}" \
+               --build-arg DESCRIPTION="${DESCRIPTION:?}" \
+               --file "${FILE:?}" \
+               --tag "${MAINTAINER:?}"/"${PROJECT:?}" .
 
    if [[ -z $CI ]];then
      mkdir -p /caches
@@ -30,13 +51,13 @@ push() {
     docker login -u "${DOCKER_LOGIN:?}" -p "${DOCKER_PASSWORD:?}"
 
     printf "\n\n--- Images ---\n\n"
-    docker images "${MAINTAINER:?}/${PROJECT}"
+    docker images "${MAINTAINER:?}/${PROJECT:?}"
 
     printf "\n\n--- Tagging ---\n\n"
-    docker tag "${MAINTAINER}/${PROJECT}:latest" "${MAINTAINER}/${PROJECT}:${TAG}"
+    docker tag "${MAINTAINER:?}/${PROJECT:?}:latest" "${MAINTAINER:?}/${PROJECT:?}:${TAG:?}"
 
     printf "\n\n--- Pushing Images to Docker Hub---\n\n"
-    docker push "${MAINTAINER}/${PROJECT}:${TAG}"
+    docker push "${MAINTAINER:?}/${PROJECT:?}:${TAG:?}"
   else
     echo "Not on master so not tagging"
     docker images
@@ -48,5 +69,5 @@ push_beta() {
 }
 
 test() {
-  docker run -it "${MAINTAINER}"/"${PROJECT}" "$1"
+  docker run -it "${MAINTAINER:?}"/"${PROJECT:?}" "${1:?}"
 }
